f1ee62e2a1b9a493e13fb2f2b106449b39b6ff63
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-04-21 18:14:29 +0300

    add seting

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+console.log(isProd);
 const nextConfig = {
+  basePath: isProd ? "/rent_car_business" : "",
   output: "export",
   distDir: "dist",
   images: {
