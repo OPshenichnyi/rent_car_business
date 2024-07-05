ca84d964f8a765788250c33c2c0ee615354dd62b
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-07-05 21:47:13 +0300

    fix bag

--- src/app/[locale]/profile/page.tsx
+++ src/app/[locale]/profile/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import styles from './page.module.css';
+
 //  getServerSession is a function that returns the session object if the user is authenticated, otherwise it returns null.
 
 export interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Profile = async ({ children }: LayoutProps) => {
+const Profile = ({ children }: LayoutProps) => {
   return (
     <div>
       <span>Dashboard</span>
