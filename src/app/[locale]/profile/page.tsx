74e5adc94e0364571cca939e9f461a88d85f9980
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-07-06 15:19:22 +0300

    profile

--- src/app/[locale]/profile/page.tsx
+++ src/app/[locale]/profile/page.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
-
 //  getServerSession is a function that returns the session object if the user is authenticated, otherwise it returns null.
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Profile: React.FC<LayoutProps> = ({ children }) => {
+const Page: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
-      <span>Dashboard</span>
+      <span>Daschboard</span>
       {children}
     </div>
   );
 };
 
-export default Profile;
+export default Page;
+
+// const Profile: React.FC<LayoutProps> = ({ children }) => {
+//   return (
+//     <div>
+//       <span>Dashboard</span>
+//       {children}
+//     </div>
+//   );
+// };
+
+// export default Profile;
 
 // https://www.youtube.com/watch?v=Eh3EpwqT4cM private route video
